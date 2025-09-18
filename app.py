dee33c9335d1b52cf21da30a5a0bcab57d55a2c8
ProfessionalGamerx <24684@burnside.school.nz>
2025-09-18 22:16:17 +1200

    accidentally opened an outdated version of my website, this commit simply reverts the commit i accidentally made using the outdated version with the most recent one.

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, g
+
 import sqlite3
+from flask import (
+    Flask, render_template, g, request, redirect, url_for, session, flash
+)
+from flask_bcrypt import Bcrypt
+from routes import routes
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+bcrypt = Bcrypt(app)
+# Path to the SQLite database file
+DATABASE = 'Backpack.db'
+
 
 
 # Helper function to query the database
 # query: SQL query string
 # args: parameters for the query
 # one: if True, return one result; else, return all
+
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-
-
-# Path to the SQLite database file
-DATABASE = 'Backpack.db'
 
 
 # Get a database connection for the current app context
@@ -36,25 +43,74 @@ def close_connection(exception):
         db.close()
 
 
-############################################################################################################
-from routes import routes
+
 app.register_blueprint(routes)
 
 
+# Register route using Flask-Bcrypt
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM Users WHERE username = ?', (username,)
+        ).fetchone()
+        if user:
+            flash('Username already exists.')
+            return redirect(url_for('register'))
+        hashed = bcrypt.generate_password_hash(password).decode('utf-8')
+        db.execute(
+            'INSERT INTO Users (username, password) VALUES (?, ?)',
+            (username, hashed)
+        )
+        db.commit()
+        flash('Registration successful! Please log in.')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+# Login route using Flask-Bcrypt
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM Users WHERE username = ?', (username,)
+        ).fetchone()
+        if user and bcrypt.check_password_hash(user['password'], password):
+            session['user_id'] = user['ID']
+            session['username'] = user['username']
+            flash('Logged in successfully!')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password.')
+    return render_template('login.html')
+
+
+# Logout route
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('Logged out.')
+    return redirect(url_for('home'))
+
+
 # Route for the home page
-@app.route("/", endpoint="home")
+@app.route('/', endpoint='home')
 def home():
     db = get_db()
     names = []
-    for table in ["Dogs", "Cats", "Other"]:
-        cur = db.execute(f"SELECT name FROM {table}")
-        names += [row["name"] for row in cur.fetchall()]
+    for table in ['Dogs', 'Cats', 'Other']:
+        cur = db.execute(f'SELECT name FROM {table}')
+        names += [row['name'] for row in cur.fetchall()]
         cur.close()
-    return render_template("index.html", pet_names=names)
+    return render_template('index.html', pet_names=names)
 
-
-###########################################################################################################
 
 # Run the Flask app in debug mode if this file is executed directly
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
